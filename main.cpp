a9851a44054ab9a6db12988183d27a8ab86e1940
Jimmy Leta <jimmy.leta@gmail.com>
2021-05-16 06:02:23 -0400

    Modified demo problem's input parameters to produce one period of a cosine wave

--- main.cpp
+++ main.cpp
@@ -13,13 +13,16 @@ int main() {
   input.mass = 1.0;
   input.stiffness = 1.0;
   input.dampening = 0.0;
-  input.num_points = 25;
+  input.num_points = 100;
   input.initial_position = 1.0;
-  input.initial_velocity = 10.0;
-  input.stop_time = 10.0;
+  input.initial_velocity = 0.0;
+  input.stop_time = 6.28;
   shmo::ShmoResults results = shmo::solve(input);
 
   std::cout << results;
   plt::plot(results.time, results.position);
+  plt::xlabel("time (seconds)");
+  plt::ylabel("position (inches)");
+  plt::grid(true);
   plt::show();
 }
