16bff4361a206c63e179fd4026d9ec7773d52a02
Jimmy Leta <jimmy.leta@gmail.com>
2021-05-16 20:16:17 -0400

    Cleaned up CMake files and added second baseline test

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+
 #include "gtest/gtest.h"
 
 #include "shmosolver.h"
@@ -8,13 +10,29 @@ TEST(BaseCases, CosineWave){
     input.mass = 1.0;
     input.stiffness = 1.0;
     input.dampening = 0.0;
-    input.num_points = 100;
+    input.num_points = 20;
     input.initial_position = 1.0;
     input.initial_velocity = 0.0;
     input.stop_time = 6.28;
     shmo::ShmoResults results = shmo::solve(input);
+    for (int i = 0; i < input.num_points; ++i) {
+        EXPECT_EQ(results.position[i], std::cos(results.time[i]));
+    }
+}
 
-    std::cout << results;
+TEST(BaseCases, SineWave){
+    shmo::ShmoInput input{};
+    input.mass = 1.0;
+    input.stiffness = 1.0;
+    input.dampening = 0.0;
+    input.num_points = 20;
+    input.initial_position = 0.0;
+    input.initial_velocity = 1.0;
+    input.stop_time = 6.28;
+    shmo::ShmoResults results = shmo::solve(input);
+    for (int i = 0; i < input.num_points; ++i) {
+        EXPECT_EQ(results.position[i], std::sin(results.time[i]));
+    }
 }
 
 int main(int argc, char *argv[]) {
